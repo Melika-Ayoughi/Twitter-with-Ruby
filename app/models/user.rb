a6d4b18eca10d4ea71900f51a20fb1cc8c542fda
MelikaAyoughi <melikaayoughi@gmail.com>
2017-06-01 18:28:32 +0430

    editing user so that it has followers and followings

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :tweets
+
+  has_many :followerships, :class_name => "Followship", :foreign_key => "following_id"
+  has_many :followers, :through => :followerships, :source => :user
+  has_many :followingships, :class_name => "Followship", :foreign_key => "follower_id"
+  has_many :followings, :through => :followingships, :source => :user
+
 end
