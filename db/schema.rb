9786f3ca4408ccff59f699ffab105dd953d762a4
MelikaAyoughi <melikaayoughi@gmail.com>
2017-06-02 15:12:01 +0430

    changing user table to have extra column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601124836) do
+ActiveRecord::Schema.define(version: 20170602102947) do
 
   create_table "followships", force: :cascade do |t|
     t.integer  "follower_id"
@@ -40,9 +40,9 @@ ActiveRecord::Schema.define(version: 20170601124836) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
 end
